2c05f814ce6f6d58c399162935e65742968cc5b2
unknown <moonkiara42@gmail.com>
2019-10-24 14:07:11 +0600

    Stock Management System

---
+++ StockManagementSystem/BLL/PurchaseManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StockManagementSystem.Model;
+using StockManagementSystem.Repository;
+using System.Data;
+
+namespace StockManagementSystem.BLL
+{
+    class PurchaseManager
+    {
+        public List<Category> GetAllCategoryFromComboBox()
+        {
+            CategoryRepository _categoryRepository = new CategoryRepository();
+            return _categoryRepository.GetAllCategoryFromComboBox();
+        }
+
+        public DataTable GetSupplierComboBox()
+        {
+
+            SupplierRepository _supplierRepository = new SupplierRepository();
+            return _supplierRepository.Display();
+        }
+
+        public List<Product> GetProductFromComboBox()
+        {
+
+            CategoryRepository _categoryRepository = new CategoryRepository();
+
+            return _categoryRepository.GetProductFromComboBox();
+
+        }
+
+        public bool ViewQuantity(string productCombo,int quantity,int prevMRP,int prevUnitPrice)
+        {
+            return true;
+        }
+
+
+
+    }
+}
---
+++ StockManagementSystem/PurchaseUI.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using StockManagementSystem.BLL;
+using StockManagementSystem.Model;
+
+namespace StockManagementSystem
+{
+    public partial class PurchaseUI : Form
+    {
+        public PurchaseUI()
+        {
+            InitializeComponent();
+        }
+
+        PurchaseManager _purchaseManager = new PurchaseManager();
+        Product product = new Product();
+
+
+        
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PurchaseUI_Load(object sender, EventArgs e)
+        {
+            categoryComboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+            categoryComboBox.DataSource = _purchaseManager.GetAllCategoryFromComboBox();
+            supplierComboBox.DataSource = _purchaseManager.GetSupplierComboBox();
+            productComboBox.DataSource = _purchaseManager.GetProductFromComboBox();
+           
+
+        }
+
+        private void categoryComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //if (categoryComboBox.SelectedValue.ToString() != null)
+            //{
+            //    int ID = Convert.ToInt32(categoryComboBox.SelectedValue.ToString());
+            //    _purchaseManager.GetProductFromComboBox(ID);
+            //    productComboBox.Enabled = true;
+               
+
+            //}
+
+
+
+        }
+
+        
+
+        private void productComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            //if (productComboBox.SelectedIndex > -1)
+            //{
+            //    productComboBox.DataSource = _purchaseManager.GetProductFromComboBox();
+
+            //    product.ID = Convert.ToInt32(productComboBox.SelectedValue);
+
+            //    categoryComboBox.Enabled = true;
+                
+            //    categoryComboBox.DataSource = _purchaseManager.GetAllCategoryFromComboBox();
+
+                
+
+
+            //    reorderLevelTextBox.Text = _stockManager.GetReorderLevel(stockIn).ToString();
+
+            //    int availableQuantity;
+            //    availableQuantity = _stockManager.GetAvailableQuantity(stockIn, stockOut);
+            //    availableQuantityTextBox.Text = availableQuantity.ToString();
+
+
+            //}
+
+
+
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
